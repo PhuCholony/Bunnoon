ceb092c3427c4b0164ff20fa0e6d1fa0fe7c7579
Phuchon Chueapong <128043114+PhuCholony@users.noreply.github.com>
2024-06-21 13:10:20 +0700

    setup @nestjs/typeorm for sqlite database
    add DB_DATABASE viable environment
    install @nestjs/typeorm typeorm sqlite3 npm packages

--- services/api/src/app/app.module.ts
+++ services/api/src/app/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
